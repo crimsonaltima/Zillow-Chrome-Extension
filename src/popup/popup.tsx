35569a0345c73361918cc179d671eb0b1c0ae3fb
crimsonaltima <crimsonaltima@gmail.com>
2023-03-28 18:40:04 -0700

    Update popup.tsx

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './popup.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./popup.css";
 
 const App: React.FC<{}> = () => {
+  const handleClick = () => {
+    // Send a message to the content script when the button is clicked
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "extractProperty" });
+    });
+  };
   return (
     <div>
       <img src="icon.png" />
+      <button onClick={handleClick}>Extract Property Info</button>
     </div>
-  )
-}
+  );
+};
 
-const container = document.createElement('div')
-document.body.appendChild(container)
-const root = createRoot(container)
-root.render(<App />)
+const container = document.createElement("div");
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(<App />);
